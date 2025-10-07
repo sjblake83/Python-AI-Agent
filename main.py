80a6ba5b87f05b7ca22684477ad2c9814d92979e
sjblake83 <sjblake83@gmail.com>
2025-10-06 20:46:22 -0400

    implement user input methods

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
+import os, sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -9,12 +9,15 @@ def main():
     api_key = os.environ.get("GEMENI_API_KEY")
     
     client = genai.Client(api_key=api_key)
-    query = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    
+    # query = input("Please enter your prompt: ")
+    query = sys.argv[1]
+    
     response = client.models.generate_content(
         model="gemini-2.0-flash-001", contents=query
     )
     
-    print(response.text)
+    print(f"Agent Response: {response.text}")
     print("\n\nUsage data:")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
