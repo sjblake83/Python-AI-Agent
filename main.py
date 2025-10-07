85e64bfad6c50ad01382bb270fd14f04895fdc13
sjblake83 <sjblake83@gmail.com>
2025-10-06 20:37:52 -0400

    establish connection with agent

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+
+
 def main():
-    print("Hello from python-ai-agent!")
+    load_dotenv()
+    api_key = os.environ.get("GEMENI_API_KEY")
+    
+    client = genai.Client(api_key=api_key)
+    query = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001", contents=query
+    )
+    
+    print(response.text)
+    print("\n\nUsage data:")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
