dad6de2bb8c27d16d1d3e6470be8efbab6cf6c7e
sjblake83 <sjblake83@gmail.com>
2025-10-07 23:19:41 -0400

    replace console output with string list

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -3,30 +3,32 @@ import os
 def get_files_info(working_directory, directory="."):
     abs_working_directory_path = os.path.abspath(working_directory)
     abs_directory_path = os.path.abspath(directory)
+    return_string = []
     
     if directory == ".":
-        print(f"Result for current directory:")
+        return_string.append(f"Result for current directory:")
     else:
-        print(f"Result for '{directory}' directory:")
+        return_string.append(f"Result for '{directory}' directory:")
      
-    
-    
     if abs_directory_path == "":
-        print(f'Error: "{directory}" is not a directory')
-        return
+        return_string.append(f'Error: "{directory}" is not a directory')
+        return return_string
     if os.path.commonpath([abs_working_directory_path, abs_directory_path]) == "":
-        print(f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
-        return
+        return_string.append(f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
+        return return_string
     
     full_path = os.path.join(
         abs_working_directory_path, 
         directory)
     directory_contents = os.listdir(full_path)
-    format_output(directory_contents, full_path)
+    return_string.extend(format_output(directory_contents, full_path))
+    return "\n".join(return_string)
 
 def format_output(directory_output, full_path):
+    return_string = []
     for output in directory_output:
         output_path = os.path.join(full_path, output)
         file_size = os.path.getsize(output_path)
         is_dir = os.path.isdir(output_path)
-        print(f" - {output}: file_size={file_size} bytes, is_dir={is_dir}")
+        return_string.append(f" - {output}: file_size={file_size} bytes, is_dir={is_dir}")
+    return return_string
