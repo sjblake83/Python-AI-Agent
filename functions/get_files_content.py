827079a655b213327f9012010cb13b21f0aae018
sjblake83 <sjblake83@gmail.com>
2025-10-19 23:17:04 -0400

    first pass on functional get_files_info function

--- functions/get_files_content.py
+++ functions/get_files_content.py
@@ -1,11 +1,11 @@
 import os
 from pathlib import Path
+from config import MAX_CHARS
 
 def get_files_info(working_directory, file_path):
     project_name = Path(__file__).resolve().parent.parent.name
     abs_working_directory_path = os.path.abspath(working_directory)
     abs_directory_path = os.path.abspath(file_path)
-    return_string = []
     
     if project_name not in os.path.commonpath([abs_working_directory_path, abs_directory_path]):
         return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
@@ -14,19 +14,18 @@ def get_files_info(working_directory, file_path):
         abs_working_directory_path, 
         file_path)
     
-    if full_path == "" or os.path.isfile(full_path):
+    if full_path == "" or not os.path.isfile(full_path):
         return f'Error: "File not found or is not a regular file: "{file_path}"'
     
-    # file reading goes here
+    return _read_file(file_path)
     
 def _read_file(file_path):
-    MAX_CHARS = 10000
-
-def _format_output(directory_output, full_path):
-    return_string = []
-    for output in directory_output:
-        output_path = os.path.join(full_path, output)
-        file_size = os.path.getsize(output_path)
-        is_dir = os.path.isdir(output_path)
-        return_string.append(f" - {output}: file_size={file_size} bytes, is_dir={is_dir}")
-    return return_string
+    with open(file_path, "r", encoding="utf-8") as f:
+        try:
+            file_content_string = f.read(MAX_CHARS)
+        except Exception as e:
+            return f'Error: {e}'
+        
+        if file_content_string.__sizeof__ == MAX_CHARS:
+            file_content_string += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+        return file_content_string
